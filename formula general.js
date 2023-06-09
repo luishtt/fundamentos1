fbd166e2a7f5ae5d798bf4523aeb4f74dfaddaa3
Luis Alberto Cedillo Martinez <127136222+luishtt@users.noreply.github.com>
2023-06-09 06:11:36 -0600

    Create formula general.js

---
+++ formula general.js
@@ -0,0 +1,9 @@
+var a = parseInt(prompt("variable a"));
+var b = parseInt(prompt("variable b"));
+var c = parseInt(prompt("variable c"));
+
+var solu1 = ((-1*-b) + (Math.sqrt((Math.pow(b,2) - (4*a*c)))))/(2*a);
+var solu2 = ((-1*-b) + (Math.sqrt((Math.pow(b,2) - (4*a*c)))))/(2*a);
+
+alert("solucion 1:"+solu1);
+alert("solucion 2:"+solu1);

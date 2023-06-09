4ad6772f0d7a21e7ea680340939859e5ec57ade9
Luis Alberto Cedillo Martinez <127136222+luishtt@users.noreply.github.com>
2023-06-09 06:27:12 -0600

    Create objetos.js

---
+++ objetos.js
@@ -0,0 +1,8 @@
+const objetos = {
+    "plato princial": "hamburguesa",
+    "postre": "elado",
+    "bebida": "jugo de naranja"
+};
+  
+const entreeValue = objetos["plato princial"]; 
+const drinkValue = objetos["bebida"]; 

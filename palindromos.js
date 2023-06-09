aa0150b84b8b1ea6ca3a7e874cce6094039badbc
Luis Alberto Cedillo Martinez <127136222+luishtt@users.noreply.github.com>
2023-06-09 06:28:56 -0600

    Create palindromos.js

---
+++ palindromos.js
@@ -0,0 +1,7 @@
+function palindromo(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+}
+palindromo("A man, a plan, a canal. Panama");
